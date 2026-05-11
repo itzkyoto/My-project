d7ac30e0975d35338b9b9faaf05cfb1895d3534f
itzkyoto <minatonamicase531@gmail.com>
2026-05-11 09:27:23 -0500

    Initial commit

---
+++ Assets/Scripts/Noce.cs
@@ -0,0 +1,51 @@
+using JetBrains.Annotations;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Noce : MonoBehaviour
+{
+
+    public int count = 10;
+
+    List<string> nombres = new List<string>() {"pepe", "maliketh", "dash", "pepe" };
+
+    string[] nombres2 = new string[4] {"pepe","maliketh","goku","ichigo" };
+    
+    public bool IsValid = false;   
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+      while (count > 0)
+        {
+            print("cuenta regresiva " + count);
+            count--;
+        }
+        while (IsValid == false)
+        {
+            print("se logro verificar al jugador");
+            IsValid = true;
+        }
+
+        int i = 0;
+
+        while (i < nombres2.Length)
+        {
+            print ("el elemento en la pocision " + i + " tiene de valor " +  nombres2[i]);
+            i++;
+
+        }
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+
+    }
+}
